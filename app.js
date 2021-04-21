07bfdac35c38a3374330ad5e230f081bf3567161
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2021-04-21 10:46:20 +0200

    login logout and newsletter + database

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const myMongo = require('mongodb').MongoClient;
+myMongo.connect("mongodb://127.0.0.1:27017", {
+useUnifiedTopology:true}).then(client => {console.log("uppkopplad mot databas");
+const myDatabase = client.db("nyhetsbrev");
+app.locals.myDatabase = myDatabase})
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const cors = require("cors"); //jag behöver köra npm install cors
 router.use(cors());
 var rand = require("random-key");
 
+
 //en klass som jag kan använda för att skapa objekt
 class User {
     constructor(id, name, password, mail, newsletter) {
@@ -16,36 +17,25 @@ class User {
     };
 };
 
-//funktion för att rega ny användare
+// funktion för att rega ny användare
 router.post('/', (req, res) => {
     let name = req.body.name;
     let password = req.body.pwd;
     let mail = req.body.email;
     let newsletter = false;
-    if(req.body.newsletter)//om det är ikryssat så skickas "on" annars undefined
-    {newsletter = true}
+    if (req.body.newsletter) //om det är ikryssat så skickas "on" annars undefined
+    {
+        newsletter = true
+    }
     let id = rand.generate(); //generera random id
     let newUser = new User(id, name, password, mail, newsletter);
-    console.log("användare från regformulär:", newUser)
     activeUser = id;
+    console.log("användare från regformulär:", newUser)
 
-
-    fs.readFile("users.json", function (err, data) {
-        if (err) {
-            console.log(err);
-        }
-
-        let users = JSON.parse(data);
-        users.push(newUser);
-
-        fs.writeFile("users.json", JSON.stringify(users, null, 2), function (err) {
-            if (err) {
-                console.log(err);
-            }
-        })
-
+    req.app.locals.myDatabase.collection("users").insertOne(newUser).then(result => {
+        console.log(result);
+        res.redirect("http://127.0.0.1:5500/?id=" + activeUser);
     })
-    res.redirect("http://127.0.0.1:5500/?id=" + activeUser); //redirectar till roten
 });
 
 //loggar ut
@@ -64,81 +54,57 @@ router.get('/activeUser', function (req, res, next) {
 
 //inloggning
 router.post('/login', (req, res) => {
-    console.log("från formuläret kommer ", req.body.name);
-    fs.readFile("users.json", function (err, data) {
-        if (err) {
-            console.log(err);
-            if (err.code == "ENOENT") {
-                console.log("Vi har tappat bort den filen");
-            }
-        }
-        activeUser = login(JSON.parse(data), req.body.name, req.body.pwd);
-        res.redirect("http://127.0.0.1:5500/?id=" + activeUser); //skickar tillbaka användare i parameter. ska ändras till id
-    });
-});
+    console.log("mottaget ", req.body.name);
+    req.app.locals.myDatabase.collection("users").find().toArray().then(users => {
+        // console.log("alla användare", users);
+        activeUser = login(users, req.body.name, req.body.pwd);
+        console.log("svar", activeUser)
+        res.json(JSON.stringify(activeUser));
+        // res.redirect("http://127.0.0.1:5500/?id=" + activeUser)
+    })
+})
 
 //en funktion som hittar användare efter id och returnerar ett objekt {namn, nyhetsbrev}
 router.get('/userData/:userId', (req, res) => {
-            fs.readFile("users.json", function (err, data) {
-                if (err) {
-                    console.log(err);
-                }
-
-                let users = JSON.parse(data);
-
-                const matchingUser = users.find(user => user.id == req.params.userId);
-                let foundUser  = {
-                        "name": matchingUser.name,
-                        "newsletter": matchingUser.newsletter
-                      }
-
-                res.json(foundUser)
-            })
-        })
-        
-
-        function login(users, userNameInput, passwordInput) {
-            for (index in users) {
-                if (users[index].name == userNameInput && users[index].pwd == passwordInput) {
-                    activeUser = users[index].id;
-                    console.log("inloggad: ", users[index].name);
-                    return activeUser
-                } else if (users[index].name == userNameInput) {
-                    // console.log("fel lösen");
-                    activeUser = "incorrect";
-                    return activeUser
-                    // console.log("fel lösen");
-                }
+    console.log(req.params.userId);
+    var query = {
+        id: req.params.userId
+    };
+    req.app.locals.myDatabase.collection("users").findOne(query)
+        .then(matchingUser => {
+            let foundUser = {
+                "name": matchingUser.name,
+                "newsletter": matchingUser.newsletter
             }
-            //console.log("fel namn");
-            activeUser = "incorrect";
-            return activeUser;
-        }
-
-
-        //tar emot ett objekt med formatet {id, newsletter}
-        //returnerar true eller false
-        router.post('/changeNewsLetter', (req, res) => {          
-            fs.readFile("users.json", function (err, data) {
-                if (err) {
-                    console.log("error :", err);
-                }
-
-                let users = JSON.parse(data);
-
-                const matchingUser = users.find(user => user.id == req.body.id);
-                if (!matchingUser) {res.send("error")}
-                //det här är som en api-nyckel. skickar man ett id som inte finns kan man inte ändra i databasen
-                let newSetting = req.body.newsletter;
-                matchingUser.newsletter= newSetting;           
-
-                fs.writeFile("users.json", JSON.stringify(users, null, 2), function (err) {
-                    if (err) {
-                        console.log(err);
-                    }
-                })        
-                res.send(JSON.stringify(matchingUser.newsletter))
-            })
+            res.json(foundUser)
         })
+})
 
-        module.exports = router;
+function login(users, userNameInput, passwordInput) {
+    for (index in users) {
+        if (users[index].name == userNameInput && users[index].pwd == passwordInput) {
+            activeUser = users[index].id;
+            console.log("inloggad: ", users[index].name);
+            return activeUser
+        } else if (users[index].name == userNameInput) {
+            // console.log("fel lösen");
+            activeUser = "incorrect";
+            return activeUser
+            // console.log("fel lösen");
+        }
+    }
+    //console.log("fel namn");
+    activeUser = "incorrect";
+    return activeUser;
+}
+
+//tar emot ett objekt med formatet {id, newsletter}
+//returnerar true eller false
+router.post('/changeNewsLetter', (req, res) => {
+    req.app.locals.myDatabase.collection("users").updateOne({"id": req.body.id}, {$set: {"newsletter": req.body.newsletter}})
+        .then(matchingUser => {console.log("tjoho", matchingUser)
+        res.send(JSON.stringify(req.body.newsletter))//behöver jag egentligen skicka något?
+    })
+})
+
+module.exports = router;
